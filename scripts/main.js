d4646fb2e64706fa70617523ad7f848d3e717265
ForasterXD <sebastiandlqs@gmail.com>
2023-12-20 19:43:29 -0400

    Enemigos Creados, sistema de colisiones primitivo creado

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
+import { Enemy } from "./enemy.js"
 import { update as updatePlayer, draw as drawPlayer } from "./player.js"
+import { checkCollision } from "./collisions.js"
 
-const canvas = document.querySelector('canvas')
+export const canvas = document.querySelector('canvas')
 canvas.width=1300
 canvas.height=731
 
@@ -8,6 +10,9 @@ const ctx = canvas.getContext('2d')
 
 let lastRenderTime = 0
 const GAMESPEED = 100
+
+const enemy = new Enemy(50, 80, 10)
+const enemy2 = new Enemy(300, 80, 5, 64, 80, false)
 
 function main(currentTime){
     window.requestAnimationFrame(main)
@@ -20,21 +25,22 @@ function main(currentTime){
         lastRenderTime = currentTime
     }
 }
-scaleCanvas()
 
 function update(){
     updatePlayer()
+    enemy.update()
+    enemy2.update()
+    if(checkCollision(enemy.x, enemy.y, enemy.WIDTH, enemy.HEIGHT, enemy2.x, enemy2.y, enemy2.WIDTH, enemy2.HEIGHT)){
+        enemy.changeDirection()
+        enemy2.changeDirection()
+    }
 }
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawPlayer(ctx)
-}
-
-function scaleCanvas(){
-    /*let scalator = screen.width / canvas.width
-    canvas.style.transform = `scale(${scalator})`
-    console.log("Escalado a: ", scalator)*/
+    enemy.draw(ctx)
+    enemy2.draw(ctx)
 }
 
 window.requestAnimationFrame(main)
--- scripts/player.js
+++ scripts/player.js
@@ -1,14 +1,20 @@
 import { getInputDirection } from "./input.js"
+import { canvas } from "./main.js"
 
 const SPEED =4
 let x = 220
-let y = 300
+let y = 620
+const WIDTH = 64
+const HEIGHT = 80
 export function update(){
     const inputDirection = getInputDirection();
-    x += inputDirection.x * SPEED; 
+    if((x + inputDirection.x * SPEED + WIDTH) < canvas.width && x + inputDirection.x * SPEED > 0){
+        //Check if player is out of the canvas first
+        x += inputDirection.x * SPEED; 
+    }
 }
 
 export function draw(gameCanvas){
     gameCanvas.fillStyle = 'blue'
-    gameCanvas.fillRect(x, y, 100, 100)
+    gameCanvas.fillRect(x, y, WIDTH, HEIGHT)
 }
